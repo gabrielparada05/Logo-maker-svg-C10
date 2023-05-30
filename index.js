415b34c4c83c38bd4cbc2668af3f605f09a58780
Gabriel Parada <gabrielparada05@gmail.com>
2023-05-30 00:48:23 -0400

    bug printing shapes. Only works triangle

--- index.js
+++ index.js
@@ -10,14 +10,6 @@ const Circle = require('./lib/shapes.js');
 
 
 
-// choice
-let answer = ""
-function isValidColor(answer) {
-  const colorNameRegex = /^(red|green|blue|yellow|magenta|cyan)$/i;
-  const hexColorRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
-  return colorNameRegex.test(answer) || hexColorRegex.test(answer);
-}
-
 
 /// Questions 
 
@@ -28,10 +20,8 @@ inquirer
       message: 'Insert an acronym for your LOGO?',
       name: 'text',
       validate: function (answer) {
-        if (answer.length < 1) {
-          return console.log("The answer must contain at least 1 character.");
-        } else  if (answer.length > 3) {
-          return console.log("The answer must contain 1 to 3 characters.");
+        if (answer.length === 0 || answer.length > 3) {
+          return console.log("The acronym should to be 1 to 3 characters");
         }
         return true;
       }
@@ -41,12 +31,12 @@ inquirer
       message: 'What color of fonts do you want? Ex: Blue, White, Red or the hexadecimal number',
       name: 'textColor',
       validate: function (answer) {
-        if (!isValidColor(answer)) {
-          return console.log(`Invalid color: "${answer}". Please provide a valid color.`);
+        if (answer.length === 0) {
+          return console.log("Write one!");
         }
         return true;
-      }
-    },
+    }
+  },
     {
       type: 'list',
       message: 'What shape do you prefer?',
@@ -66,8 +56,8 @@ inquirer
       message: 'What color of shape do you want? Ex: Blue, White, Red or the hexadecimal number',
       name: 'patternColor',
       validate: function (answer) {
-        if (!isValidColor(answer)) {
-          return console.log(`Invalid color: "${answer}". Please provide a valid color.`);
+        if (answer.length === 0) {
+          return console.log("Write one!");
         }
         return true;
       }
@@ -76,18 +66,24 @@ inquirer
 
   ///generate response and print the svg file
   .then((response) => {
-    let svgCode = ""
+    
+    console.log(response);
+    
+    let svgCode = "";
+    let patternChoice = response.pattern  
 
-    if (response.pattern === "Triangle") {
-      svgCode += new Triangle(0,0,`${response.patternColor}`, `${response.patternColor}`,5, `${response.text}`, `${response.textColor}`, 50, 20, 100, 100, 10, 100).render();
-    } else if (response.pattern  === "Square") {
-      svgCode += new Square(10, 10, `${response.patternColor}`, `${response.patternColor}`, 5, `${response.text}`, `${response.textColor}`, 50, 50).render();
+    if (patternChoice === "Triangle") {
+       svgCode = new Triangle(0,0,`${response.patternColor}`, `${response.patternColor}`,5, `${response.text}`, `${response.textColor}`, 50, 20, 100, 100, 10, 100).render();
+    } else if (patternChoice  === "Square") {
+       svgCode = new Square(10, 10, `${response.patternColor}`, `${response.patternColor}`, 5, `${response.text}`, `${response.textColor}`, 50, 50).render();
+    } else if  (patternChoice === "Circle")  {
+       svgCode = new Circle(0, 0, `${response.patternColor}`, `${response.patternColor}`, 3, `${response.text}`, `${response.textColor}`, 30, 50, 50).render();
     } else {
-      svgCode += new Circle(0, 0, `${response.patternColor}`, `${response.patternColor}`, 3, `${response.text}`, `${response.textColor}`, 30, 50, 50).render();
+      console.log("Invalid pattern")
     }
-
-    // let textPick = `<text x="50" y="80" text-anchor="middle" dominant-baseline="middle" fill="${response.textColor}">${response.text}</text> </svg>`
-
+   
+  
+    console.log(svgCode)
    
   
     ///print  
